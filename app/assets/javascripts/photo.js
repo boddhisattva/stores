dbc22ffefa242253374e6e4b2864ece63be63731
boddhisattva <mail4mohnish@gmail.com>
2013-08-25 09:58:29 +0530

    adding carrierwave

---
+++ app/assets/javascripts/photo.js
@@ -0,0 +1,12 @@
+//$(function () {
+////    var url =  '/api/v2/photos';
+//
+//})
+
+//$("#new_photo").fileupload() - throwing error will see later..
+//        {
+//            url: url
+//      }
+
+
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :division_id, :model, :name, :price
+  attr_accessible :division_id, :model, :name, :price, :image
   belongs_to :division, :counter_cache => true
+  mount_uploader :image, ImageUploader
 end
